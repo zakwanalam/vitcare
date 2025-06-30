7158393f6f61c7bc2921e4ccd583f8ba14893928
Zakwan Alam <zakwan267@github.com>
2025-06-30 16:57:39 +0500

    fixed deployment errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+/** @type {import ('next').NextConfig} */
+
 const nextConfig: NextConfig = {
   /* config options here */
+    async redirects(){
+      return [
+        {
+          source:'/',
+          destination :'/Home',
+          permanent:true
+        }
+      ]
+    }
 };
 
 export default nextConfig;
